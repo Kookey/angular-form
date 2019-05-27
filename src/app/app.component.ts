9db9413aa0daec9d65288305b3375d2154ea0278
Kookey <wxlhdm@qq.com>
2019-05-27 20:13:05 +0800

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,18 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+export class Address {
+   street: string = '';
+   city: string = '';;
+   state: string = '';;
+   zip: string = '';;
+}
+
+export class Profile {
+  firstName: string = '';;
+  lastName: string = '';;
+  address: Address = new Address();
+}
 
 @Component({
   selector: 'my-app',
@@ -7,9 +21,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Angular';
+  domain = new Profile();
+
+  profileForm: FormGroup;
+
+
+  constructor(private fb: FormBuilder){}
 
   ngOnInit(){
-    this.name = "Test";
+    const pr = new Profile();
+     console.warn(pr);
+    const domainSetting: Object = Reflect.getPrototypeOf(pr);
+    console.warn(domainSetting);
+    this.profileForm = this.fb.group(domainSetting);
+    this.domain.firstName = 'xingl';
+    this.domain.address.city = 'sh';
+    console.log(this.domain);
+    console.log('111');
+    this.profileForm.patchValue(this.domain);
     
   }
 }
